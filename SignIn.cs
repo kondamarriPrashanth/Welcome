996c9d5bacfeed35ce02baa3109f5e5af5d6436e
abc <abc@gmail.com>
2024-02-15 12:07:17 +0530

    finall commit

---
+++ SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+   public  class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "user name and password required ";
+            }
+            else
+            {
+                if((userName == "admin") && ( password == "admin123@"))
+                {
+                    msg = "Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentcation fail";
+                }
+            }
+            return "provide user details";
+        }
+    }
+}
